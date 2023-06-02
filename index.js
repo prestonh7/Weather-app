e2d7561d2c9c76a337108f23eb8ff06e5cd2f90a
prestonh7 <prestonhouse7@gmail.com>
2023-06-02 15:03:05 -0500

    Weather is pushed to dom

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 async function getWeather(location) {
     try {
-        const request = await fetch('http://api.weatherapi.com/v1/forecast.json?key=6bc1f1ea845c4991a09232222232905&q=' + location + '&days=7&aqi=no&alerts=no');
+        const request = await fetch('http://api.weatherapi.com/v1/forecast.json?key=&q=' + location + '&days=7&aqi=no&alerts=no');
         const weatherData = await request.json();
         const weatherArray = weatherData.forecast.forecastday;
         console.log(weatherArray);
@@ -16,22 +16,38 @@ async function drawWeather(weatherData) {
         const weatherArray = await weatherData;
 
         weatherArray.forEach(item => {
+            const icon = item.day.condition.icon;
             const date = item.date;
             const avgTemp = item.day.avgtemp_f;
             const minTemp = item.day.mintemp_f;
             const maxTemp = item.day.maxtemp_f;
-            const maxWind = item.day.maxwind_mph;
 
-            const dataString = `Date: ${date}, Avg Temp: ${avgTemp}°F, Min Temp: ${minTemp}°F, Max Temp: ${maxTemp}°F Max Wind speed: ${maxWind} mph`;
-            const paragraph = document.createElement('p');
-            paragraph.textContent = dataString;
-            const content = document.getElementById('content');
-            content.appendChild(paragraph);
+            createWeatherCard(icon, date, avgTemp, minTemp, maxTemp);
         });
     }
     catch(err) {
         console.log('Error: ' + err);
     }
+}
+
+function createWeatherCard(icon, date, avgTemp, minTemp, maxTemp) {
+    const content = document.getElementById('content');
+    const div = document.createElement('div');
+    const image = document.createElement('img');
+    const dateText = document.createElement('p');
+    const avg = document.createElement('p');
+    const range = document.createElement('p');
+
+    image.src = `https:${icon}`;
+    dateText.innerText = `${date}`;
+    avg.innerText = `${avgTemp}`
+    range.innerText = `${minTemp} - ${maxTemp}`;
+
+    div.appendChild(dateText);
+    div.appendChild(image);
+    div.appendChild(avg);
+    div.appendChild(range);
+    content.appendChild(div);
 }
 
 drawWeather(getWeather('houston'));
