1fd723fc001f0a8bd3cbf2374f8fdfc302533f97
prestonh7 <prestonhouse7@gmail.com>
2023-06-01 20:47:25 -0500

    Fixed appendChild null error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 async function getWeather(location) {
     try {
-        const request = await fetch('http://api.weatherapi.com/v1/forecast.json?key=&q=' + location + '&days=7&aqi=no&alerts=no');
+        const request = await fetch('http://api.weatherapi.com/v1/forecast.json?key=6bc1f1ea845c4991a09232222232905&q=' + location + '&days=7&aqi=no&alerts=no');
         const weatherData = await request.json();
         const weatherArray = weatherData.forecast.forecastday;
         console.log(weatherArray);
@@ -11,10 +11,8 @@ async function getWeather(location) {
     }
 }
 
-
 async function drawWeather(weatherData) {
     try {
-        const content = document.querySelector('.content');
         const weatherArray = await weatherData;
 
         weatherArray.forEach(item => {
@@ -27,6 +25,7 @@ async function drawWeather(weatherData) {
             const dataString = `Date: ${date}, Avg Temp: ${avgTemp}°F, Min Temp: ${minTemp}°F, Max Temp: ${maxTemp}°F Max Wind speed: ${maxWind} mph`;
             const paragraph = document.createElement('p');
             paragraph.textContent = dataString;
+            const content = document.getElementById('content');
             content.appendChild(paragraph);
         });
     }
